18d7606f9438e1b825643cf0c6c3d6441903a4cd
kazimirovic <kazimirovic@protonmail.com>
2019-02-24 20:59:31 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='torrent-bencode',
-    version='0.1',
+    version='0.2',
     packages=['bencode'],
     url='https://github.com/kazimirovic/bencode',
     license='MIT',
